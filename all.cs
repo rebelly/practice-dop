4efaba60fa32a8587144b47d812f9307683e06e4
Sergey <114340735+rebelly@users.noreply.github.com>
2022-11-01 11:37:38 +0300

    Create all.cs

---
+++ all.cs
@@ -0,0 +1,112 @@
+using System;
+public class Program
+{
+    static string period(int m, int n)
+    {
+        string remain = "";
+        if ((m / n) * n == m) {
+            return "0";
+        }
+        else
+        {
+
+            m -= m / n * n;
+            for (int i = 0; i <= 50; i++)
+            {
+
+                if ((m / n) == 0)
+
+                {
+                    if (m * 10 / n > 0)
+                    {
+                        m *= 10;
+                        continue;
+                    }
+                    remain += "0";
+                    m *= 10;
+                }
+
+                else
+                {
+                    remain += $"{m / n}";
+                    m -= (m / n) * n;
+                }
+
+
+            }
+            return remain;
+        }
+    }
+    static void fath_son(int g_f, int g_s)
+    {
+        int fath_w= 0;
+        int son_w = 0;
+        for (int i = 300; i <= 400; i++)
+        {
+            if (g_f % i == 0 && g_s%i == 0)
+            {
+                fath_w = g_f / i;
+                son_w = g_s / i;
+            }
+        }
+        Console.WriteLine($"{fath_w} {son_w}");
+    }
+    static int nod(int a, int b)
+    {
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a -= b;
+            }
+            else
+            {
+                b -= a;
+            }
+        }
+        return a;
+    }
+    static void frac(int a, int b, int c , int d)
+    {
+        int  den = (a * d + c * b) ;
+        int numer = d * b;
+        int NOD= nod(den, numer);
+        den /= NOD;
+        numer /= NOD;
+
+        Console.WriteLine($"{den}/{numer}");
+    }
+    static int n_step_m(int n, int m){
+      int close = m;
+      int close_step = 1;
+      int step = 1;// n -число m - степень
+      while (close >= n){
+        close *= m;
+      }
+      
+      for (int i = 0; i < close; i++){
+        close_step *= m;
+      }
+      return n - close_step;    }
+    public static void Main()
+    {
+        /* int m = int.Parse(Console.ReadLine());        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Остаток от деления числа {m} на {n} равен {period(m, n)}");
+*/        
+/*        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        frac(a, b, c, d);
+        
+        */
+        int m = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+      Console.WriteLine($"{n_step_m(m,n)}");
+        /*
+        int m = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        fath_son(m,n);
+          */
+    }
+}
